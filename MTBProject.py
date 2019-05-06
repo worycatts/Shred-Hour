798fb174749c10ff4e564e56a06d0d828e85bcf4
killawatts13 <seedubya13@gmail.com>
2019-05-06 19:23:45 -0400

    Updated helper functions. Working on parsing data.

--- MTBProject.py
+++ MTBProject.py
@@ -2,40 +2,39 @@
 This is a short script to search for local places to ride and
 provide weekend weather for the local trail selected.
 """
-import urllib3
 import yaml
-from helper import get_zipcode_data, write_config, read_config
+from helper import where_is_you, get_trails, get_weather
 
-
+# importing config to access values
 with open("config.yaml", 'r') as yamlfile:
     cfg = yaml.safe_load(yamlfile)
 
+# values
 weatherauth = (cfg['darksky']['apikey'])
 mtbauth = (cfg['mtbproject']['apikey'])
 zipcode = (cfg['profile']['zipcode'])
 
+# If location is not available for ease of access later
+# ~~~~~~~ NEED TO COMPLETE THIS FUNCTION so it writes zipcode, lat & long to config.yaml file  ~~~~~~~
+if zipcode is None:
+    where_is_you()
 
-def where_is_you():
-    if zipcode is None:
-        # requesting zipcode data to find your location in relation to the trails!
-        data = get_zipcode_data(input("Looks like we don't know where you are. "
-                                      "Please enter your zipcode to provide reference: "))
-        print(data)
-
-
-where_is_you()
+# values from zip code
 lat = (cfg['profile']['latitude'])
 long = (cfg['profile']['longitude'])
 dist = (cfg['profile']['distance'])
 
-http = urllib3.PoolManager()
-trails = http.request('GET', 'https://www.mtbproject.com/data/get-trails?lat=%s&lon=%s&maxDistance=%s&key=%s'
-                      % (lat, long, dist, mtbauth))
-print(trails.data)
-
-forecast = http.request('GET', "https://api.darksky.net/forecast/%s/%s,%s" % (weatherauth, lat, long))
-
-print(forecast.data)
+trails = get_trails(lat, long, dist, mtbauth)
+print(trails)
+print("---------")
+print(trails.values())
 
 
+"""
+print(get_trails(lat, long, dist, mtbauth))
+print('=================================')
 
+weather = get_weather(weatherauth, lat, long)
+print(weather)
+type(weather)
+"""
--- helper.py
+++ helper.py
@@ -1,5 +1,20 @@
+import urllib3
 import yaml
+from pandas import json
 from uszipcode import SearchEngine
+
+
+
+def read_config(data):
+    with open('config.yml', 'r') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+    outfile.close()
+
+
+def write_config(data):
+    with open('config.yml', 'w') as outfile:
+        yaml.dump(data, outfile, default_flow_style=False)
+    outfile.close()
 
 
 def get_zipcode_data(zipcode):
@@ -7,14 +22,24 @@ def get_zipcode_data(zipcode):
     data = search.by_zipcode(str(zipcode))
     return data
 
-def write_config(data):
-    with open('config.yml', 'w') as outfile:
-        yaml.dump(data, outfile, default_flow_style=False)
-    outfile.close()
 
-def read_config(data):
-    with open('config.yml', 'r') as outfile:
-        yaml.dump(data, outfile, default_flow_style=False)
-    outfile.close()
+# ~~~~~~~ NEED TO COMPLETE THIS FUNCTION so it writes zipcode, lat & long to config.yaml file  ~~~~~~~
+def where_is_you():
+    # requesting zipcode data to find your location in relation to the trails!
+    return get_zipcode_data(input("Looks like we don't know where you are. "
+                                  "Please enter your zipcode to provide reference: "))
+
+
+def get_trails(lat, long, dist, mtb_auth):
+    http = urllib3.PoolManager()
+    url = http.request('GET', 'https://www.mtbproject.com/data/get-trails?lat=%s&lon=%s&maxDistance=%s&key=%s'
+                       % (lat, long, dist, mtb_auth))
+    data = json.loads(url.data.decode('utf-8'))
+    return data
+
+
+def get_weather(weather_auth, lat, long):
+    http = urllib3.PoolManager()
+    return http.request('GET', "https://api.darksky.net/forecast/%s/%s,%s" % (weather_auth, lat, long))
 
 
